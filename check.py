01cb2d356436a5fc57516085dafe65a0e7cff4a1
abel ashenafi <ashenafiabel66@gmail.com>
2025-07-31 23:09:51 +0300

    test with directed graph

--- check.py
+++ check.py
@@ -1,20 +1,7 @@
 import pickle
-import pprint
 
-def inspect_pickle_file(pkl_path):
-    with open(pkl_path, "rb") as f:
-        data = pickle.load(f)
-    
-    print("Type of root object:", type(data))
-    print("\nSample content preview:\n")
-    
-    # Pretty print the first few keys/items depending on the type
-    if isinstance(data, dict):
-        pprint.pprint({k: data[k] for k in list(data)[:5]})
-    elif isinstance(data, list):
-        pprint.pprint(data[:5])
-    else:
-        pprint.pprint(data)
+with open("directed.pkl", "rb") as f:
+    data = pickle.load(f)
 
-if __name__ == "__main__":
-    inspect_pickle_file("graph.pkl")
+print(type(data))
+print(data.keys() if isinstance(data, dict) else "Not a dict")
--- common/data.py
+++ common/data.py
@@ -134,19 +134,24 @@ class CustomGraphDataset:
             return pickle.load(f)
 
     def _build_graph(self):
-        G = nx.DiGraph() if self.directed else nx.Graph()
-        G.add_nodes_from(self.raw_data['nodes'])
+        # Case 1: raw_data is already a NetworkX graph (Graph or DiGraph)
+        if isinstance(self.raw_data, nx.Graph):
+            G = self.raw_data.copy()
+        # Case 2: raw_data is a dict with 'nodes' and 'edges'
+        elif isinstance(self.raw_data, dict) and 'nodes' in self.raw_data and 'edges' in self.raw_data:
+            G = nx.DiGraph() if self.directed else nx.Graph()
+            G.add_nodes_from(self.raw_data['nodes'])
+            G.add_edges_from(self.raw_data['edges'])
+        else:
+            raise ValueError("Unsupported graph format in pickle file.")
 
-        cleaned_edges = []
-        for edge in self.raw_data['edges']:
-            if len(edge) == 3:
-                u, v, attr = edge
-                cleaned_attr = {k: v for k, v in attr.items() if isinstance(v, (int, float))}
-                cleaned_edges.append((u, v, cleaned_attr))
-            else:
-                cleaned_edges.append(edge)
-        G.add_edges_from(cleaned_edges)
+        # Ensure directionality matches user request
+        if self.directed and not G.is_directed():
+            G = G.to_directed()
+        elif not self.directed and G.is_directed():
+            G = G.to_undirected()
 
+        # Add dummy node features if missing
         for node in G.nodes():
             if 'node_feature' not in G.nodes[node]:
                 G.nodes[node]['node_feature'] = torch.tensor([1.0], dtype=torch.float)
